332c1c1b375a378287b3b14935b8701d083a61b4
k.dobrogost <dante.3.kd@gmail.cojm>
2019-11-23 23:36:58 +0100

    strona

---
+++ BeetRootDoctorPage/BeetRootDoctorPage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BeetRootDoctorPage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
