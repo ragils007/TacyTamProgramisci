fbd9855f3a7adcce453d1d99ab5339f48ff17d76
t-gordzielik <tomasz.gordzielik@gmail.com>
2019-11-24 07:47:52 +0100

    kamera

--- BeetRootDoctorPage/BeetRootDoctorPage/Controllers/HomeController.cs
+++ BeetRootDoctorPage/BeetRootDoctorPage/Controllers/HomeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using BeetRootDoctorPage.Models;
+using Db.Pgsql;
+using Db.Pgsql.Factory;
 
 namespace BeetRootDoctorPage.Controllers
 {
@@ -23,6 +25,20 @@ namespace BeetRootDoctorPage.Controllers
         public IActionResult History()
         {
             return View("History");
+        }
+
+        [HttpPost]
+        public IActionResult Save(string img)
+        {
+            var dd = img.Split(',');
+            var teraz = DateTime.Now;
+            string filePath = $"{teraz.Year}_{teraz.Month}_{teraz.Day}_{teraz.Hour}_{teraz.Minute}_{teraz.Second}_{teraz.Millisecond}.jpg";
+            System.IO.File.WriteAllBytes(filePath, Convert.FromBase64String(dd[1]));
+            var db = new Postgres();
+            var log = new cameralogFactory(db);
+            log.Add(1, 1, filePath,"1","1","nazwa");
+
+            return Ok();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
